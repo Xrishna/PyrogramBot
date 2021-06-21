54ffdaadcd1c8e2bbbe1f5b6bcc32b14f7cb4f93
Krishna <82990200+Xrishna@users.noreply.github.com>
2021-06-21 10:55:10 +0530

    Update start.py

--- PyroBot/plugins/start.py
+++ PyroBot/plugins/start.py
@@ -4,6 +4,14 @@ from pyrogram import Client, filters
 async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
-        text="Hello there!",
+        text="Its Just A Pyrogram Test Bot Of @Itsxrishna",
         reply_to_message_id=update.message_id
     )
+
+    @Client.on_message(filters.command(["rate"]))
+async def start(bot, update):
+    await bot.forward_messages(
+    chat_id=update.chat.id,
+    from_chat_id=-1001159467641,
+    message_ids=3
+)
